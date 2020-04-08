5282f0391070b109b7333f2d8815ffc491235b41
monicaoyugi <monicaoyugi@gmail.com>
2020-04-08 22:22:47 +0300

    configured database

--- HealthS/settings.py
+++ HealthS/settings.py
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'HealthS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+if os.environ.get('MODE') == "dev":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'PORT': os.environ.get('DB_PORT'),
+            'NAME': os.environ.get('DB_NAME'),
+            'USER':os.environ.get('DB_USER'),
+            'PASSWORD':os.environ.get('DB_PASSWORD'),
+            'HOST': os.environ.get('DB_HOST'),
+        }
 }
+#production
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+        )
+    }
 
 
 # Password validation
